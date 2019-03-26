7d76c4d89850412c4edf01901945dce0beac8d1d
Joshua Ransom <keylog0728@gmail.com>
2019-03-26 10:06:48 -0400

    Initial Commit

---
+++ src/main/kotlin/com/joshua/ransom/dbproject/DbprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.joshua.ransom.dbproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbprojectApplication>(*args)
+}
