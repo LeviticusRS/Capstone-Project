75107cfb47b7df480e4ab6dc771b9e81da8d1d1c
Joshua Ransom <keylog0728@gmail.com>
2019-04-23 09:44:52 -0400

    Main starting class added

--- src/main/kotlin/com/joshua/ransom/dbproject/DbprojectApplication.kt
+++ src/main/kotlin/com/joshua/ransom/dbproject/DbprojectApplication.kt
@@ -1,11 +1,15 @@
 package com.joshua.ransom.dbproject
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
 class DbprojectApplication
 
 fun main(args: Array<String>) {
-	runApplication<DbprojectApplication>(*args)
+    runApplication<DbprojectApplication>(*args)
 }
