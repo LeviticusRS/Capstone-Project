286cfec6a61db662d67595982f6dd1ff18c1eb43
Joshua Ransom <keylog0728@gmail.com>
2019-04-23 20:55:40 -0400

    Ability to delete Actors, Paths updated

--- src/main/kotlin/com/joshua/ransom/dbproject/controller/ActorController.kt
+++ src/main/kotlin/com/joshua/ransom/dbproject/controller/ActorController.kt
@@ -4,28 +4,35 @@ import com.joshua.ransom.dbproject.model.Actor
 import com.joshua.ransom.dbproject.model.Gender
 import com.joshua.ransom.dbproject.repository.ActorRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.text.SimpleDateFormat
 
 @RestController
+@RequestMapping("/api/actor")
 class ActorController {
 
     @Autowired
     lateinit var actorRepository: ActorRepository
 
-    private final fun deploy() {
+    @GetMapping("/save")
+    private final fun deploy(): String {
         var timestamp = SimpleDateFormat("dd-MM-yyyy").parse("07-01-1996").time / 1000
         actorRepository.save(Actor("Joshua Ransom", timestamp, Gender.MALE.value))
         timestamp = SimpleDateFormat("dd-MM-yyyy").parse("04-05-1990").time / 1000
         actorRepository.save(Actor("Zion Williamson", timestamp, Gender.FEMALE.value))
+        return "Done - Actors"
     }
 
     @RequestMapping("/findAllActors")
     fun findAll() = actorRepository.findAll()
 
-    @RequestMapping("findByActorName/{actorName}")
+    @RequestMapping("/deleteActorById/{id}")
+    fun deleteActorById(@PathVariable actorId: Long) = actorRepository.deleteById(actorId)
+
+    @RequestMapping("/findByActorName/{actorName}")
     fun findByActorName(@PathVariable actorName: String) = actorRepository.findByActorName(actorName)
 
 }
